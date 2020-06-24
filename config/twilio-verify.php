d3bf328d0bac44bfc008057f000a856fc547350f
Ivan Sotelo <isotelo@controlla.com.mx>
2020-06-23 21:40:33 -0600

    feat: provider and config

---
+++ config/twilio-verify.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Twilio Keys
+    |--------------------------------------------------------------------------
+    |
+    | The Twilio publishable sid, token and verify sid key give you access to
+    | Twilio's API. The "publishable" key is typically used when interacting
+    | with Twilio.js while the "token" key accesses private API endpoints.
+    |
+    */
+
+    'sid' => env('TWILIO_SID'),
+
+    'token' => env('TWILIO_AUTH_TOKEN'),
+
+    'verify_sid' => env('TWILIO_VERIFY_SID'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Twilio Verify Model
+    |--------------------------------------------------------------------------
+    |
+    | This is the model in your application that implements the MustVerifyPhone
+    | trait provided by Twilio Verify. It will serve as the primary model you
+    | use while interacting with Twilio Verify related methods.
+    |
+    */
+
+    'model' => env('TWILIO_VERIFY_MODEL', App\User::class),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Twilio Logger
+    |--------------------------------------------------------------------------
+    |
+    | This setting defines which logging channel will be used by the Twilio
+    | library to write log messages. You are free to specify any of your
+    | logging channels listed inside the "logging" configuration file.
+    |
+    */
+
+    'logger' => env('TWILIO_VERIFY_LOGGER'),
+
+];
---
+++ src/TwilioVerifyServiceProvider.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace IvanSotelo\TwilioVerify;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class TwilioVerifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerLogger();
+        // $this->registerRoutes();
+        // $this->registerResources();
+        $this->registerMigrations();
+        $this->registerPublishing();
+    }
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->configure();
+        $this->bindLogger();
+    }
+
+    /**
+     * Setup the configuration for TwilioVerify.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/twilio-verify.php', 'twilio-verify'
+        );
+    }
+
+    /**
+     * Bind the Openpay logger interface to the TwilioVerify logger.
+     *
+     * @return void
+     */
+    protected function bindLogger()
+    {
+        $this->app->bind(LoggerInterface::class, function ($app) {
+            return new Logger(
+                $app->make('log')->channel(config('twilio-verify.logger'))
+            );
+        });
+    }
+
+    /**
+     * Register the Openpay logger.
+     *
+     * @return void
+     */
+    protected function registerLogger()
+    {
+        if (config('twilio-verify.logger')) {
+            //Stripe::setLogger($this->app->make(LoggerInterface::class));
+        }
+    }
+
+    /**
+     * Register the package routes.
+     *
+     * @return void
+     */
+    protected function registerRoutes()
+    {
+        if (TwilioVerify::$registersRoutes) {
+            Route::group([
+                'prefix' => config('twilio-verify.path'),
+                'namespace' => 'IvanSotelo\TwilioVerify\Http\Controllers',
+                'as' => 'twilio-verify.',
+            ], function () {
+                $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+            });
+        }
+    }
+
+    /**
+     * Register the package resources.
+     *
+     * @return void
+     */
+    protected function registerResources()
+    {
+        $this->loadJsonTranslationsFrom(__DIR__.'/../resources/lang');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'twilio-verify');
+    }
+
+    /**
+     * Register the package migrations.
+     *
+     * @return void
+     */
+    protected function registerMigrations()
+    {
+        if (TwilioVerify::$runsMigrations && $this->app->runningInConsole()) {
+            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        }
+    }
+
+    /**
+     * Register the package's publishable resources.
+     *
+     * @return void
+     */
+    protected function registerPublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/twilio-verify.php' => $this->app->configPath('twilio-verify.php'),
+            ], 'twilio-verify-config');
+
+            $this->publishes([
+                __DIR__.'/../database/migrations' => $this->app->databasePath('migrations'),
+            ], 'twilio-verify-migrations');
+
+            // $this->publishes([
+            //     __DIR__.'/../resources/views' => $this->app->resourcePath('views/vendor/twilio-verify'),
+            // ], 'twilio-verify-views');
+        }
+    }
+}
